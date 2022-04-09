100562766c3dcf8f589747dd2219189e88e9a4b9
Parisheelan Negi <parisheelannegi@gmail.com>
2022-04-09 21:52:34 +0530

    minor adjustments

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+//Dark Mode Toggle
+
 const body = document.querySelector("body");
 const toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
+
+// Dark Mode that changes changes according to device preference
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  body.classList.add("dark");
+}
+if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+  body.classList.remove("dark");
+}
